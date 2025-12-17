350a0ce114e41cb15a204e363370751ae607f092
Thomas McManamen <tm38@illinois.edu>
2025-12-17 16:39:42 -0600

    pins, config, and basic mcu test

--- src/pins.h
+++ src/pins.h
@@ -1,62 +1,50 @@
 #pragma once
 
 // SPI sensor bus
-#define SPI_MISO 39
-#define SPI_MOSI 37
-#define SPI_SCK 38
+#define SPI_MISO 3
+#define SPI_MOSI 2
+#define SPI_SCK 1
 
 // barometer chip select
-#define MS5611_CS 43
+#define MS5611_CS 44
 
-// gyro chip select
-#define LSM6DS3_CS 36
-
-// high g chip select
-#define KX134_CS 40
-
-// low g chip select
-#define ADXL355_CS 44
+// imu chip select
+#define LSM6DSV320X_CS 43
 
 // magnetometer chip select
-#define LIS3MDL_CS 35
-
-// orientation chip select, interrupt
-#define BNO086_CS 48
-#define BNO086_INT 34
-#define BNO086_RESET 33
+#define MMC5983_CS 4
 
 // i2c bus pins
-#define I2C_SDA 10
-#define I2C_SCL 9
+#define I2C_SDA 21
+#define I2C_SCL 26
 
 // buzzer pin
-#define BUZZER_PIN 17
+#define BUZZER_PIN 12
 #define BUZZER_CHANNEL 1
 
-// CAN pins
-#define CAN_CS 2
-#define RFM96W_CS 5
-
 // GPS I2C location
-#define GNSS_I2C_LOCATION 0x3A
-#define GPS_RESET GpioAddress(2, 017)
+#define GNSS_I2C_LOCATION 0x3A //??
+#define GPS_RESET 5
 #define GPS_ENABLE 0
 
-#define FLASH_DAT0 13
-#define FLASH_DAT1 12
-#define FLASH_DAT2 15
-#define FLASH_DAT3 16
-#define FLASH_CLK 18
-#define FLASH_CMD 14
+// Flash
+#define FLASH_DAT0 33
+#define FLASH_DAT1 34
+#define FLASH_DAT2 18
+#define FLASH_DAT3 17
+#define FLASH_CLK 16
+#define FLASH_CMD 47
 
-#define E22_CS 5
-#define E22_DI01 4
-#define E22_DI03 3
-#define E22_BUSY 6
-#define E22_RXEN 7
-#define E22_RESET 8
+// LoRa
+#define E22_CS 37
+#define E22_DI01 41
+#define E22_DI03 40
+#define E22_BUSY 38
+#define E22_RXEN 39
+#define E22_RESET 6
 
-#define INA745_ADDR 0x44
-
-#define PYRO_SDA 41
-#define PYRO_SCL 42
+// LEDs
+#define LED_RED 11
+#define LED_ORANGE 10
+#define LED_GREEN 9
+#define LED_BLUE 8
